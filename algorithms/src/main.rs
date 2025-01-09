b388a40275801c0e5b373d97eebbb8b51c07103d
JoelEarps <joeldearps@gmail.com>
2025-01-09 18:46:47 +0000

    feat: bubble sort algorithm

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -1,4 +1,4 @@
-
+mod sort_alogrithms;
 
 fn main() {
     println!("Hello, world!");
--- algorithms/src/sort_alogrithms/bubble_sort.rs
+++ algorithms/src/sort_alogrithms/bubble_sort.rs
@@ -10,20 +10,45 @@ Disadvantages:
 1. Slow
 */
 
-fn bubble_sort(array_to_be_sorted: Vec<i64>) ->  Vec<i64> {
+pub fn bubble_sort(array_to_be_sorted: &mut Vec<i64>) {
     let mut swapped = true;
     while swapped {
         swapped = false;
-        for item in 0..array_to_be_sorted.len() - 1 {
-            
+        for vector_element_index in 0..array_to_be_sorted.len() - 1 {
+            if array_to_be_sorted[vector_element_index] > array_to_be_sorted[vector_element_index + 1] {
+                println!("Smaller value detected to the right of a larger value, swapping...");
+                array_to_be_sorted.swap(vector_element_index, vector_element_index + 1);
+                swapped = true;
+            } else {
+                println!("Larger or Equal value detected to the right of a smaller value, continuing...");
+            }
         }
     }
 }
 
-#[cfg(tests)]
-mod tests {
-    #[test]
-    fn test_bubble_sort(){
 
+#[cfg(test)]
+mod tests {
+    use super::bubble_sort;
+    #[test]
+    fn test_bubble_sort_no_change(){
+        let mut array_under_test: Vec<i64> = vec![1, 2, 3];
+        bubble_sort(&mut array_under_test);
+        // assert_eq!(sorted_array, &mut vec![1, 2, 3]);
+        println!("{:?}", array_under_test);
+    }
+
+    #[test]
+    fn test_bubble_sort_changes(){
+        let mut array_under_test: Vec<i64> = vec![5, 3, 4, 1, 9];
+        bubble_sort(&mut array_under_test);
+        assert_eq!(array_under_test, [1, 3, 4, 5, 9]);
+    }
+
+    #[test]
+    fn test_bubble_equal_values(){
+        let mut array_under_test: Vec<i64> = vec![1, 1, 1];
+        bubble_sort(&mut array_under_test);
+        assert_eq!(array_under_test, [1, 1, 1]);
     }
 }
