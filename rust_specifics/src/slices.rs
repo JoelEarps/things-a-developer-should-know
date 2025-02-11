dff9773275511fa269857d2cf582aa898917de85
Joel Earps <joel.earps@swissborg.com>
2025-02-11 21:05:54 +0000

    feat: slices

--- rust_specifics/src/slices.rs
+++ rust_specifics/src/slices.rs
@@ -8,7 +8,34 @@ write a function that takes a string of words separated by spaces and returns th
 
 // Range syntax
 // You can define start and end points but if you are referencing the start or end you can leave blank
-// let slice = &s[0..2]; - definitive start and end
-// let slice = &s[..2]; - assume at start
-// let slice = &s[3..]; - assume end
-// let slice = &s[..]; - slice of entire string
+fn string_slice_demo(example_string: String){
+    let slice = &example_string[0..2]; // definitive start and end
+    println!("{slice}");
+    let slice = &example_string[..2]; // assume at start
+    println!("{slice}");
+    let slice = &example_string[3..]; // assume end
+    println!("{slice}");
+    let slice = &example_string[..]; // slice of entire string
+    println!("{slice}");
+}
+
+fn array_slice_demo(){
+    let test_array = [1, 2, 3, 4, 5];
+    let test_slice = &test_array[..3];
+    println!("{:?}", test_slice);
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_string_slice(){
+        string_slice_demo(String::from("Hello, Joel"));
+    }
+
+    #[test]
+    fn test_array_slice(){
+        array_slice_demo();
+    }
+}
