5c88899a9385f7711afbca26d97ce7058a66e328
Joel Earps <joel.earps@swissborg.com>
2025-05-08 21:43:44 +0100

    feat: initial binary search

--- algorithms/src/search_alogrithms/binary_search.rs
+++ algorithms/src/search_alogrithms/binary_search.rs
@@ -14,48 +14,56 @@ There are three examples below:
 3. Using the binary search function available in the std lib
  */
 
-
+#[derive(Debug)]
 struct TradeFee {
     volume: usize, 
     total_price: usize
 }
 
-pub type UserTradeFees = Vec<TradeFee>;
+type UserTradeFees = Vec<TradeFee>;
 
-
-// The problem arose when I was trying to look for where a trade amount fit in
-fn find_trade_amount(tiers: UserTradeFees, target_volume: usize) -> Option<usize> {
-    if tiers.is_empty() {
+// The problem arose when I was trying to look for where a trade amount fit in when looking for associated prices
+fn find_trade_amount(trade_fees: &[TradeFee], target_volume_for_placement: &usize) -> Option<usize> {
+    if trade_fees.is_empty() {
         return None;
     }
 
-    let mut low = 0;
-    let mut high = tiers.len() - 1;
+    let mut lower_bound = 0;
+    let mut higher_bound = trade_fees.len() - 1;
     let mut result: Option<usize> = None;
 
-    while low <= high {
-        println!("Low is : {}, high is :{} ", low, high);
-        let mid = (low + high) / 2;
-        println!("Mid is : {} ", mid);
-        let ntl_cutoff = &tiers[mid].ntl_cutoff
+    while lower_bound <= higher_bound {
+        
+        println!("The Lower bounds (index) of the search area {}, is currently less than or equal to the higher bounds (index) of the search area ({})", lower_bound, higher_bound);
+        let mid = (lower_bound + higher_bound) / 2;
+        println!("The mid point of the search vec is at index: {} ", mid);
+        let current_mid_volume_under_search = &trade_fees[mid].volume;
         println!(
-            "Is target volume {}, larger than {}",
-            target_volume, ntl_cutoff
+            "Is target volume {}, larger than the current volume at the mid point of the vector being searched {}",
+            target_volume_for_placement, current_mid_volume_under_search
         );
-        if target_volume >= &ntl_cutoff {
-            println!("{:?}", tiers[mid]);
-            let value = &tiers[mid].volume;
-            result = Some(value);
-            low = mid + 1;
+        if target_volume_for_placement >= &current_mid_volume_under_search {
+            println!("Yes it is, setting the current result is bigger than the higher bound, setting the new low bound to be the index after the current midpoint, therefore new search area is {} to {}", mid + 1 , higher_bound);
+            let value = &trade_fees[mid].volume;
+            result = Some(value.clone());
+            lower_bound = mid + 1;
         } else {
             if mid == 0 {
-                tracing::warn!("Trade volume is the lower than the value required for the minimum VIP level");
-                result = Some(&tiers[mid].volume);
+                println!("The mid point of the search area is 0, therefore we cannot search anymore, therefore the volume should be placed at the start of the array");
+                let value = &trade_fees[mid].volume;
+                result = Some(value.clone());
                 break;
             }
-            high = mid - 1;
+            println!("No it is not, setting the new higher bound to be one below the current middle index, therefore new search area is {} to {}", lower_bound, mid -1);
+            higher_bound = mid - 1;
         }
     }
+    println!("We have reached the end of the binary search algorithm (where either the lower bound is now larger than the higher bound, meaning we place the , the end volume is {:?}", result);
+    // The output here is contextual to the search
+    // 1. The output is the volume 
+    // 2. If the output volume is the same as the first index volume, then we insert at the start of the input array
+    // 3. If the output volume is 
+    // To handle this you could return an enum where it tells you where to insert - between to values, at the start or at the end
     result
 }
 
@@ -89,7 +97,20 @@ mod binary_search_tests {
 
     use super::*;
     
+    #[test]
     fn raw_binary_search(){
-
+        // Could use RSTest here, but not the point right now, maybe add at the end
+        let target_volume_1: usize = 6;
+        let entry_volume_to_be_inserted_after = find_trade_amount(test_struct, &target_volume_1);
+        assert!(entry_volume_to_be_inserted_after.is_some());
+        assert_eq!(entry_volume_to_be_inserted_after.unwrap(), 5);
+        let target_volume_1: usize = 0;
+        let entry_volume_to_be_inserted_after = find_trade_amount(test_struct, &target_volume_1);
+        assert!(entry_volume_to_be_inserted_after.is_some());
+        assert_eq!(entry_volume_to_be_inserted_after.unwrap(), 1);
+        let target_volume_1: usize = 10;
+        let entry_volume_to_be_inserted_after = find_trade_amount(test_struct, &target_volume_1);
+        assert!(entry_volume_to_be_inserted_after.is_some());
+        assert_eq!(entry_volume_to_be_inserted_after.unwrap(), 7);
     }
 }
